df29e959e117c4947e5760123ff4e19f38f48f5e
GAMIDAm <miguel.bap.job@gmail.com>
2025-10-30 15:06:33 -0300

    altersção

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,36 +3,42 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-function convertPokemonToli(pokemon){
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => {
+        return `
+        <li class="type">${typeSlot.type.name}</li>
+        `
+    })
+}
+
+function convertPokemonToli(pokemon) {
     return `
     <li class="pokemon">
-                    <span class="number">#001</span>
+                    <span class="number">#${pokemon.order}</span>
                     <span class="name">${pokemon.name}</span>
                     
                     <div class="detail">
                         <ol class="types">
-                            <li class="type">grass</li>
-                            <li class="type">poison</li>
+                            ${convertPokemonTypesToLi(pokemon.types).join('')}
                         </ol>
 
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}"/>
+                        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}"/>
                     </div>    
                 </li>`
 }
 
 const pokemonList = document.getElementById('pokemonList');
 
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.then((pokemons) => {
-    
-for (let i = 0; i < pokemons.length; i++) {
-    const pokemon = pokemons[i];
+PokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToli).join('');
+    /*  
+    const listItens = [];
 
-    
-    pokemonList.innerHTML += convertPokemonToli(pokemon);
-}
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listItens.push(convertPokemonToli(pokemon));
+    }
 
+
+    console.log(listItens); */
 })
-.catch((error) => console.log(error))
